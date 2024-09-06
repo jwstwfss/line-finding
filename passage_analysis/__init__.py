8af3075a27ee08adff3eb20217b254dd37ddf6c9
ayanacharyya <acharyyaayan@gmail.com>
2024-09-06 14:33:38 +0200

    removed commented out imports

--- passage_analysis/__init__.py
+++ passage_analysis/__init__.py
@@ -7,9 +7,7 @@ import pickle
 import os
 import astropy.io.ascii as asciitable
 import astropy.io.fits as fits
-#import time
 import mpfit
-#import multiprocessing as mp
 import math
 from scipy import interpolate
 from scipy import integrate
@@ -18,11 +16,9 @@ from trim_spec import trim_spec
 from utilities import gaussian
 from utilities import is_number
 from utilities import read_config
-#from specmodel import emissionline_model_spline
-#from specmodel import model_resid_spline
 from find_cwt import find_cwt
 from find_cwt  import loop_field_cwt
-#from find_cwt import test_obj_cwt # AA commented this out as it seems to be unused
+
 from fitting import emissionline_model
 from fitting import model_resid
 from fitting import fit_obj
@@ -32,18 +28,3 @@ from fitting import get_fitpar_indices
 from guis import *
 from measure_z_interactive import *
 import pickle
-#from gather_secure_sample import *
-#from measure_stack import *
-#from dustfromstack import *
-#from mle_stack import *
-
-
-
-# try:
-#     from stacking import *
-# except ImportError:
-#     pass
-# #    print 'No stacking module. It is not needed for line finding. Skipping'
-
-import warnings
-warnings.filterwarnings('ignore')
